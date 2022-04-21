2f8246cfa80bb5e7311f4a8ef3b2b402473fb84e
oleesir <oleesir@gmail.com>
2022-04-21 09:07:06 +0100

    updated the schema

--- src/database/model/carModel.js
+++ src/database/model/carModel.js
@@ -3,10 +3,8 @@ import { Schema, model } from "mongoose";
 const carSchema = new Schema(
 	{
 		photo: { type: String, required: true },
-		name: { type: String, required: true },
-		condition: { type: String, enum: ["New", "Used"], default: "New", required: true },
+		brand: { type: String, required: true },
 		model: { type: String, required: true },
-		year: { type: Number, required: true },
 		price: { type: Number, required: true },
 		sellerName: { type: String, required: true },
 		sellerNumber: { type: String, required: true },
